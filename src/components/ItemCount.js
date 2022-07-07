9d2792d46250843af9e45eee7dc55c0ed0c45567
BRY <rogerllanos061308@gmail.com>
2022-07-07 00:39:13 -0300

    se agrego itemcount y callback

---
+++ src/components/ItemCount.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+
+const ItemCount = ({stock,initial,onAdd}) => {
+    const [count, setCount]= useState(initial);
+    
+    const incrementar = () => {
+        setCount(count + 1);
+    };
+    const decrementar = () => {
+        setCount((valAnt) => (valAnt > 0 ? count - 1 : count));
+    };
+    return (
+    <div>
+        <h3>{count}</h3>
+        <button onClick={decrementar}>-</button>
+        <button onClick={incrementar}>+</button>
+        <button onClick={() => {
+            if (count <= stock){
+                onAdd(count);
+            }
+        }}>Agregar</button>
+    </div>
+  )
+}
+
+export default ItemCount
