0fbdcd2badde5e83f9660773e7eb7862a2bec547
BRY <rogerllanos061308@gmail.com>
2022-07-07 00:41:42 -0300

    se agrega callback al itemlistcontainer

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import ItemCount from './ItemCount'
 
 const ItemListContainer = ({greeting}) => {
+  function onAddCallBack(n) {
+    alert(`agregados ${n} productos`);
+  }
   return (
-    <div> {greeting}</div>
+    <div> 
+      {greeting}
+      <ItemCount stock={5} initial={1} onAdd={onAddCallBack} />
+    </div>
   )
 }
 
